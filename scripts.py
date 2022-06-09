cd8f7d91ff3000df849c1d8344868da154f9887f
CosmicOrder <Matrixxturn@yandex.ru>
2022-06-09 14:51:37 +0300

    refactor: took out repeated pieces of code in the function

--- scripts.py
+++ scripts.py
@@ -6,42 +6,7 @@ from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from datacenter.models import Schoolkid, Mark, Chastisement, Lesson, \
     Commendation
 
-
-def fix_marks(schoolkid: str):
-    if schoolkid == '':
-        return 'Пустая строка не принимается, введите имя и фамилию'
-    try:
-        marks = Mark.objects.filter(schoolkid__full_name__icontains=schoolkid)
-    except ObjectDoesNotExist:
-        print('Ученика с таким именем не существует. Уточните '
-              'правильность написания имени')
-    except MultipleObjectsReturned:
-        print('Учеников с таким именем слишком много, уточните фамилию '
-              'ученика')
-    else:
-        bad_marks = marks.filter(points__in=[2, 3])
-        for mark in bad_marks:
-            mark.points = randint(4, 5)
-            mark.save()
-
-
-def remove_chastisements(schoolkid: str):
-    if schoolkid == '':
-        return 'Пустая строка не принимается, введите имя и фамилию'
-    try:
-        child = Schoolkid.objects.get(full_name__icontains=schoolkid)
-    except ObjectDoesNotExist:
-        print('Ученика с таким именем не существует. Уточните '
-              'правильность написания имени')
-    except MultipleObjectsReturned:
-        print('Учеников с таким именем слишком много, уточните фамилию '
-              'ученика')
-    else:
-        comments = Chastisement.objects.filter(schoolkid=child)
-        comments.delete()
-
-
-praise_text = """
+PRAISE_TEXT = """
     1. Молодец!
     2. Отлично!
     3. Хорошо!
@@ -73,10 +38,7 @@ praise_text = """
     29. Ты многое сделал, я это вижу!
     30. Теперь у тебя точно все получится!"""
 
-praise_phrases = re.split('\n\s+\d+\.\s+', praise_text)
-praise_phrases = list(filter(None, praise_phrases))
-
-subjects = [
+SUBJECTS = [
     'Краеведение',
     'География',
     'Математика',
@@ -93,34 +55,64 @@ subjects = [
     'Основы безопасности жизнедеятельности (ОБЖ)',
 ]
 
+praise_phrases = re.split('\n\s+\d+\.\s+', PRAISE_TEXT)
+praise_phrases = list(filter(None, praise_phrases))
 
-def create_commendation(schoolkid: str, subject: str):
+
+def get_child(schoolkid: str):
     if schoolkid == '':
         return 'Пустая строка не принимается, введите имя и фамилию'
-
-    if subject not in subjects:
-        return 'Такого прдемета не существует, проверьте правильность ' \
-               'написания предмета'
     try:
         child = Schoolkid.objects.get(full_name__icontains=schoolkid)
+        return child
     except ObjectDoesNotExist:
         print('Ученика с таким именем не существует. Уточните '
               'правильность написания имени')
     except MultipleObjectsReturned:
         print('Учеников с таким именем слишком много, уточните фамилию '
               'ученика')
-    else:
-        target_lessons = Lesson.objects.filter(
-            year_of_study=child.year_of_study,
-            group_letter=child.group_letter,
-            subject__title__icontains=subject)
 
-        target_lesson = choice(target_lessons)
 
-        Commendation.objects.create(
-            text=choice(praise_phrases),
-            created=target_lesson.date,
-            schoolkid=child,
-            subject=target_lesson.subject,
-            teacher=target_lesson.teacher,
-        )
+def fix_marks(schoolkid: str):
+    child = get_child(schoolkid)
+    if not child:
+        return
+
+    marks = Mark.objects.filter(schoolkid__full_name__icontains=child)
+    for mark in marks.filter(points__in=[2, 3]):
+        mark.points = randint(4, 5)
+        mark.save()
+
+
+def remove_chastisements(schoolkid: str):
+    child = get_child(schoolkid)
+    if not child:
+        return
+    comments = Chastisement.objects.filter(schoolkid=child)
+    comments.delete()
+
+
+def create_commendation(schoolkid: str, subject: str):
+    child = get_child(schoolkid)
+    if not child:
+        return
+
+    elif subject not in SUBJECTS:
+        return 'Такого прдемета не существует, проверьте правильность ' \
+               'написания предмета'
+
+    target_lessons = Lesson.objects.filter(
+        year_of_study=child.year_of_study,
+        group_letter=child.group_letter,
+        subject__title__icontains=subject,
+    )
+
+    target_lesson = choice(target_lessons)
+
+    Commendation.objects.create(
+        text=choice(praise_phrases),
+        created=target_lesson.date,
+        schoolkid=child,
+        subject=target_lesson.subject,
+        teacher=target_lesson.teacher,
+    )
